a311f4a26bb64a2832f26339032490e66cc2e769
elijah <elijahlucian@gmail.com>
2024-12-09 12:13:28 -0700

    quick setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Page from './components/Page'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { RootRouter } from './RootRouter'
+import { Theme } from '@radix-ui/themes'
+import { ThemeProvider } from '@mui/material'
+import { theme } from './lib/theme'
+import { ApiContextProvider } from './lib/contexts/ApiContext'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env.local file')
+}
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: 0 } },
+})
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Page />
-    </>
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      signInFallbackRedirectUrl={'/'}
+    >
+      <QueryClientProvider client={queryClient}>
+        <ApiContextProvider>
+          <ThemeProvider theme={theme}>
+            <Theme radius="large" accentColor="blue">
+              <RootRouter />
+            </Theme>
+          </ThemeProvider>
+        </ApiContextProvider>
+      </QueryClientProvider>
+    </ClerkProvider>
   )
 }
 
---
+++ src/RootRouter.tsx
@@ -0,0 +1,63 @@
+import { SignIn, useAuth, UserButton } from '@clerk/clerk-react'
+import { Route, Routes } from 'react-router-dom'
+import { Flex, Grid, Spinner } from '@radix-ui/themes'
+import { Lawyers } from './pages/Lawyers'
+import { UnauthedDashboard } from './pages/UnauthedDashboard'
+import { Example } from './pages/Example'
+import { AppHeader } from './components/layout/AppHeader'
+import { Link } from './components/Link'
+import { Divider, Typography } from '@mui/material'
+
+export const RootRouter = () => {
+  const auth = useAuth()
+
+  if (!auth.isLoaded)
+    return (
+      <Grid>
+        <Spinner />
+      </Grid>
+    )
+
+  return auth.isSignedIn ? (
+    <Grid>
+      <AppHeader>
+        <Flex gap="3" align="end">
+          <Typography variant="h5">Lexter Lens</Typography>
+          <Link to="/">Dashboard</Link>
+          <Link to="/lawyers">Lawyers</Link>
+        </Flex>
+        <UserButton />
+      </AppHeader>
+      <Divider />
+      <Routes>
+        <Route path="/lawyers" element={<Lawyers />} />
+        <Route path="/*" element={<>Dashboard</>} />
+      </Routes>
+    </Grid>
+  ) : (
+    <Grid>
+      <AppHeader>
+        <Flex gap="3" align="end">
+          <Link to="/">
+            <Typography variant="h5">Lexter Lens</Typography>
+          </Link>
+          <Link to="/example">Example</Link>
+        </Flex>
+        <Link to="/sign-in">Sign In</Link>
+      </AppHeader>
+      <Divider />
+      <Routes>
+        <Route path="example" element={<Example />} />
+        <Route
+          path="/sign-in"
+          element={
+            <Grid width="100%" height="100%">
+              <SignIn />
+            </Grid>
+          }
+        />
+        <Route path="/*" element={<UnauthedDashboard />} />
+      </Routes>
+    </Grid>
+  )
+}
