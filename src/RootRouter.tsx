9b66679c1bc00f99858a1ac8e93c6b798cc779f5
hychen958 <hychen958@gmail.com>
2024-12-13 00:48:59 -0700

    Create initial routes

--- src/RootRouter.tsx
+++ src/RootRouter.tsx
@@ -1,37 +1,52 @@
-import { SignIn, useAuth, UserButton } from '@clerk/clerk-react'
-import { Route, Routes } from 'react-router-dom'
-import { Flex, Grid, Spinner } from '@radix-ui/themes'
-import { Lawyers } from './pages/Lawyers'
-import { UnauthedDashboard } from './pages/UnauthedDashboard'
-import { Example } from './pages/Example'
-import { AppHeader } from './components/layout/AppHeader'
-import { Link } from './components/Link'
-import { Divider, Typography } from '@mui/material'
+import { SignIn, useAuth, UserButton } from '@clerk/clerk-react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { Flex, Grid, Spinner } from '@radix-ui/themes';
+import { Lawyers } from './pages/Lawyers';
+import { UnauthedDashboard } from './pages/UnauthedDashboard';
+import { Example } from './pages/Example';
+import { AppHeader } from './components/layout/AppHeader';
+import { Link } from './components/Link';
+import { Divider, Typography } from '@mui/material';
+
+// Dummy components for new pages
+const Home = () => <div>Home/Dashboard Page</div>;
+const QAContent = () => <div>QA + Discussion Page</div>;
+const SingleQA = () => <div>Single QA + Discussion Page</div>;
+const ErrorPage = () => <div>Error Page</div>;
+const Registration = () => <div>Registration Page</div>;
+const Settings = () => <div>Settings Page</div>;
 
 export const RootRouter = () => {
-  const auth = useAuth()
-
-  if (!auth.isLoaded)
+  const auth = useAuth();
+  if (!auth.isLoaded) {
     return (
       <Grid>
         <Spinner />
       </Grid>
-    )
+    );
+  }
 
   return auth.isSignedIn ? (
     <Grid>
       <AppHeader>
         <Flex gap="3" align="end">
           <Typography variant="h5">Lexter Lens</Typography>
-          <Link to="/">Dashboard</Link>
+          <Link to="/home">Dashboard</Link>
           <Link to="/lawyers">Lawyers</Link>
+          <Link to="/content">QA + Discussion</Link>
+          <Link to="/settings">Settings</Link>
         </Flex>
         <UserButton />
       </AppHeader>
       <Divider />
       <Routes>
+        <Route path="/home" element={<Home />} />
         <Route path="/lawyers" element={<Lawyers />} />
-        <Route path="/*" element={<>Dashboard</>} />
+        <Route path="/content" element={<QAContent />} />
+        <Route path="/qa" element={<SingleQA />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/error" element={<ErrorPage />} />
+        <Route path="*" element={<Navigate to="/error" replace />} />
       </Routes>
     </Grid>
   ) : (
@@ -47,17 +62,12 @@ export const RootRouter = () => {
       </AppHeader>
       <Divider />
       <Routes>
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/registration" element={<Registration />} />
         <Route path="example" element={<Example />} />
-        <Route
-          path="/sign-in"
-          element={
-            <Grid width="100%" height="100%">
-              <SignIn />
-            </Grid>
-          }
-        />
-        <Route path="/*" element={<UnauthedDashboard />} />
+        <Route path="/error" element={<ErrorPage />} />
+        <Route path="*" element={<Navigate to="/error" replace />} />
       </Routes>
     </Grid>
-  )
-}
+  );
+};
