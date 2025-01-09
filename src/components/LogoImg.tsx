30b782d98275c32765f157ca042c4cd659168e99
BMHDI <167380565+BMHDI@users.noreply.github.com>
2025-01-09 13:41:31 -0700

    initial commit LXDV-30 (#7)
    * initial commit
    
    * fixed spacing ...
    
    * Fixeing If statement in email verficaqtion
    
    * made the changes after the PR

--- src/components/LogoImg.tsx
+++ src/components/LogoImg.tsx
@@ -1,9 +1,16 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-export const LogoImg = () => {
+interface LogoImgProps {
+  variant?: 'Dark' | 'Light'; 
+  Size? :  number 
+}
+
+export const LogoImg: React.FC<LogoImgProps> = ({ variant = 'Light' , Size = 25 }) => {
+  const logoSrc = variant === 'Dark' ? '../../assets/logoDark.svg' : '../../assets/logoLight.svg';
+ 
   return (
     <Link to="/">
-      <img height={'25px'} src="../../assets/logo.svg" alt="logo" />
+      <img height={Size} src={logoSrc} alt={`logo-${variant}`} />
     </Link>
-  )
-}
+  );
+};
