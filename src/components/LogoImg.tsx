ab45c6862dcb7f3f376d803aab6bd661af0f8853
BMHDI <167380565+BMHDI@users.noreply.github.com>
2025-01-12 19:43:44 -0700

    Lxdv 31 (#10)
    * initial commit
    
    * Removed the sign up
    
    * added Steps instead of Back button
    
    * Some styling
    
    * Some styling

--- src/components/LogoImg.tsx
+++ src/components/LogoImg.tsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
+import logoDark from '../../assets/logoDark.svg';
+import logoLight from '../../assets/logoLight.svg';
 
 interface LogoImgProps {
   variant?: 'Dark' | 'Light'; 
   Size? :  number 
 }
 
-export const LogoImg: React.FC<LogoImgProps> = ({ variant = 'Light' , Size = 25 }) => {
-  const logoSrc = variant === 'Dark' ? '../../assets/logoDark.svg' : '../../assets/logoLight.svg';
+export const LogoImg: React.FC<LogoImgProps> = ({ variant = 'Light', Size = 25 }) => {
+  const logoSrc = variant === 'Dark' ? logoDark : logoLight;
  
   return (
     <Link to="/">
