de69c6e12390a671278a3de7be94e9dd988134c7
BMHDI <167380565+BMHDI@users.noreply.github.com>
2025-01-12 19:43:44 -0700

    Lxdv 31 (#10)
    * initial commit
    
    * Removed the sign up
    
    * added Steps instead of Back button
    
    * Some styling
    
    * Some styling

--- src/components/FirstSigninFlow.tsx
+++ src/components/FirstSigninFlow.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, ChangeEvent } from 'react'
 import {
   Dialog,
   DialogTitle,
@@ -8,61 +8,81 @@ import {
   Button,
   IconButton,
   Box,
-} from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
-import { LogoImg } from './LogoImg';
+  Divider,
+  Link,
+  Typography,
+} from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
+import { LogoImg } from './LogoImg'
 
 interface SignUpForm {
-  email: string;
-  name: string;
-  verificationCode?: string;
+  email: string
+  name: string
+  verificationCode?: string
 }
 
 interface FirstSigninFlowProps {
-  isFirstSignIn: boolean;
-  setIsFirstSignIn: (value: boolean) => void;
+  isFirstSignIn: boolean
+  setIsFirstSignIn: (value: boolean) => void
 }
 
-const FirstSigninFlow: React.FC<FirstSigninFlowProps> = ({ isFirstSignIn, setIsFirstSignIn }) => {
-  const [step, setStep] = useState<number>(1);
+const FirstSigninFlow: React.FC<FirstSigninFlowProps> = ({
+  isFirstSignIn,
+  setIsFirstSignIn,
+}) => {
+  const [step, setStep] = useState<number>(1)
   const [formData, setFormData] = useState<SignUpForm>({
     email: '',
     name: '',
     verificationCode: '',
-  });
+  })
 
   const handleClose = () => {
-    setIsFirstSignIn(false);
-  };
+    setIsFirstSignIn(false)
+  }
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData(prevFormData => ({
+    const { name, value } = e.target
+    setFormData((prevFormData) => ({
       ...prevFormData,
       [name]: value,
-    }));
-  };
+    }))
+  }
 
   const handleNextStep = () => {
-    setStep(prevStep => prevStep + 1);
-  };
+    setStep((prevStep) => prevStep + 1)
+  }
 
   const handlePreviousStep = () => {
-    setStep(prevStep => prevStep - 1);
-  };
+    setStep((prevStep) => prevStep - 1)
+  }
 
   return (
     <Dialog fullWidth open={isFirstSignIn} onClose={handleClose}>
       <Box
         component="section"
-        sx={{ p: 4, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}
+        sx={{
+          p: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
       >
         <LogoImg Size={60} variant="Dark" />
       </Box>
       <DialogTitle
         sx={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center' }}
       >
-        {`Step ${step} / 4: ${step === 1 ? 'Enter your Email and Password' : step === 2 ? 'Verify your Email' : step === 3 ? 'Enter your Name' : 'Complete your Sign-Up'}`}
+        {`${
+          step === 1
+            ? 'Lexter Lens Quickstart'
+            : step === 2
+              ? `Let's Make Lexter Lens Relevant to You`
+              : step === 3
+                ? 'Make the Legal World More Transparent'
+                : 'Completed'
+        }`}
       </DialogTitle>
       <IconButton
         aria-label="close"
@@ -71,7 +91,7 @@ const FirstSigninFlow: React.FC<FirstSigninFlowProps> = ({ isFirstSignIn, setIsF
           position: 'absolute',
           right: 8,
           top: 8,
-          color: theme => theme.palette.grey[500],
+          color: (theme) => theme.palette.grey[500],
         }}
       >
         <CloseIcon />
@@ -79,34 +99,39 @@ const FirstSigninFlow: React.FC<FirstSigninFlowProps> = ({ isFirstSignIn, setIsF
       <DialogContent>
         {step === 1 && (
           <>
-            <TextField
-              fullWidth
-              margin="normal"
-              type="email"
-              name="email"
-              label="Email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-            <TextField
-              fullWidth
-              margin="normal"
-              type="password"
-              name="password"
-              label="Password"
-            />
+            <p>
+              {' '}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
           </>
         )}
         {step === 2 && (
-          <TextField
-            fullWidth
-            margin="normal"
-            type="text"
-            name="verificationCode"
-            label="Verification Code"
-            value={formData.verificationCode}
-            onChange={handleChange}
-          />
+          <>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <TextField
+              fullWidth
+              margin="normal"
+              type="text"
+              name="verificationCode"
+              label="Verification Code"
+              value={formData.verificationCode}
+              onChange={handleChange}
+            />
+            <Divider />
+              <TextField
+              fullWidth
+              margin="normal"
+              type="text"
+              name="verificationCode"
+              label="Verification Code"
+              value={formData.verificationCode}
+              onChange={handleChange}
+            />
+          </>
         )}
         {step === 3 && (
           <TextField
@@ -121,12 +146,12 @@ const FirstSigninFlow: React.FC<FirstSigninFlowProps> = ({ isFirstSignIn, setIsF
         )}
       </DialogContent>
       <DialogActions>
-        {step > 1 && <Button onClick={handlePreviousStep}>Back</Button>}
-        {step < 4 && <Button onClick={handleNextStep}>Next</Button>}
-        {step === 4 && <Button onClick={handleClose}>Complete Sign-Up</Button>}
+        {step > 1 && <Link onClick={handlePreviousStep} ><Typography sx={{fontWeight: 'light'}}>Step: {step-1}/3</Typography></Link>}
+        {step < 4 && <Button sx={{fontWeight: 'bold'}} onClick={handleNextStep}>Next</Button>}
+        {step === 4 && <Button  sx={{fontWeight: 'bold'}} onClick={handleClose}>Complete Sign-Up</Button>}
       </DialogActions>
     </Dialog>
-  );
-};
+  )
+}
 
-export default FirstSigninFlow;
+export default FirstSigninFlow
