6d43190c05fb19e9719be31147bc6819197f9801
Petra Petlanova <petrapetlanova@gmail.com>
2025-01-13 09:33:41 -0700

    Lxdv 40 (#14)
    * Created a SearchBar component.
    
    * created a responsive search bar, moved to nav bar.
    
    * import { useState } from 'react';
    
    * Fixed home page merge conflicts.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,16 @@
-import { Page } from "../components/layout/Page"
-
+import React, { useState } from 'react';
+import { Page } from "../components/layout/Page";
 
 const Home: React.FC = () => {
-  // State to track if it's the first sign-in
   const [isFirstSignIn, setIsFirstSignIn] = useState<boolean>(true);
 
-  // Function to handle resetting first sign-in status
   const handleTestClick = () => {
     setIsFirstSignIn(true);
   };
 
   return (
     <Page>
-
+      
     </Page>
   );
 };
