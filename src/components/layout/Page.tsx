aa4be7b8a0356ddc1fbda8870967ea62d0217f28
hychen958 <hychen958@gmail.com>
2025-01-21 12:14:31 -0700

    page adjustment

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -5,12 +5,10 @@ export const Page = styled(Flex)({
   flexDirection: 'column',
   alignItems: 'center',
   height: '100vh',
-  width: '90%',
+  width: '90%',  //
   maxWidth: '1200px',
   margin: '0 auto',
   padding: 20,
-  marginTop:60,
-  backgroundColor: '#fff',
+  marginTop:60,//
   boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
-  borderRadius: '8px',
 });
