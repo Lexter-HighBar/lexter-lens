ac7de6fdcc89bb89ebc9b75eb2a98d45671518fb
Hank Chen <hychen958@gmail.com>
2025-01-21 20:32:38 -0700

    Revert "Lxdv 48 (#24)"
    This reverts commit 340c64761c52d73bfaff61489616f0695b416f1c.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,5 @@
-import { Paper, styled } from '@mui/material';
+import { Paper, styled } from '@mui/material'
 
 export const Card = styled(Paper)({
-  padding: 20,
-  backgroundColor: '#fff',
-  boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
-  borderRadius: '8px',
-});
+  padding: 2,
+})
--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -1,14 +1,10 @@
-import { styled } from '@mui/material';
-import { Flex } from '@radix-ui/themes';
+import { styled } from '@mui/material'
+import { Flex } from '@radix-ui/themes'
 
 export const Page = styled(Flex)({
   flexDirection: 'column',
   alignItems: 'center',
+  // justifyContent: 'center',
   height: '100vh',
-  width: '90%',  //
-  maxWidth: '1200px',
-  margin: '0 auto',
   padding: 20,
-  marginTop:60,//
-  boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
-});
+})
