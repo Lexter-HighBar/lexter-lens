1dbbf9cb98ec2195e80542ba10b541bd38d99819
Tarik Boumehdi <167380565+BMHDI@users.noreply.github.com>
2025-01-26 21:25:08 -0700

    Update UserdataApiContext.tsx

--- src/lib/contexts/UserdataApiContext.tsx
+++ src/lib/contexts/UserdataApiContext.tsx
@@ -17,7 +17,7 @@ export const UserDataApiProvider = ({ children }: Props) => {
   const { getToken } = useAuth();
   const axios = Axios.create();
 
-  // Base URL for userdata API
+  // Base URL for userdata API 
   axios.defaults.baseURL =  'https://lexter-server.onrender.com/api'; //To be replaced after adding .env with the next line 
  // axios.defaults.baseURL = import.meta.env.VITE_EXPRES_API_URL || '/userdata';
 
