e695383dcf15ae1c630e68d0d0bf561c2ed1a0fd
Tarik Boumehdi <167380565+BMHDI@users.noreply.github.com>
2025-02-03 21:16:05 -0700

    Lxdv 80 (#49)
    * tags Ai
    
    * Added Auto complete from MUI
    
    * Updated some styling
    
    * Update QuestionItem.tsx

--- src/RootRouter.tsx
+++ src/RootRouter.tsx
@@ -14,6 +14,7 @@ import Signin from './pages/Sign-in'
 import Discussion from './pages/Discussion'
 import UserProfile from './components/UserProfile'
 import { UnauthedDashboard } from './pages/UnauthedDashboard'
+import QuestionView from './pages/QuestionView'
 
 // RootRouter Component
 export const RootRouter = () => {
@@ -44,8 +45,8 @@ export const RootRouter = () => {
             </RequireAuth>
           }
         />
-   
-          <Route
+
+        <Route
           path="/discussion"
           element={
             <RequireAuth>
@@ -59,6 +60,14 @@ export const RootRouter = () => {
           element={
             <RequireAuth>
               <UserProfile />{' '}
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/question/:id"
+          element={
+            <RequireAuth>
+              <QuestionView />
             </RequireAuth>
           }
         />
