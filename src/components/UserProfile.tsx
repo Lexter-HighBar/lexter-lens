8cbb7a4c429cb2e22e008dd566c4abcb2c214d14
Tarik <tarikboumehdi91@gmail.com>
2025-02-17 21:41:14 -0700

    update style and reusable cmpnt

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -6,27 +6,17 @@ import {
   Alert,
   Box,
   Typography,
-  Card,
-  MenuItem,
-  Chip,
-  Select,
-  SelectChangeEvent,
 } from '@mui/material'
-
-interface City {
-  id: number
-  city: string
-}
 
 import Grid from '@mui/material/Grid2'
 import { useState, useEffect } from 'react'
-import { useTags } from '../hooks/useTags'
-import { useCities } from '../hooks/useCities'
+
+import { Page } from './layout/Page'
+import TagSelector from './tagsSelector'
 
 const UserProfile = () => {
   const { user } = useUser()
-  const { tags } = useTags()
-  const { cities } = useCities()
+
   const [avatar, setAvatar] = useState(user?.imageUrl || '')
   const [selectedTags, setSelectedTags] = useState<string[]>([])
   const [selectedCities, setSelectedCities] = useState<string[]>([])
@@ -83,14 +73,6 @@ const UserProfile = () => {
     reader.readAsDataURL(file)
   }
 
-  const handleTagChange = (event: SelectChangeEvent<string[]>) => {
-    setSelectedTags(event.target.value as string[])
-  }
-
-  const handleRemoveTag = (tagToRemove: string) => {
-    setSelectedTags((prevTags) => prevTags.filter((tag) => tag !== tagToRemove))
-  }
-
   const handleUpdateTags = async () => {
     setLoading(true)
     try {
@@ -117,16 +99,6 @@ const UserProfile = () => {
     } finally {
       setLoading(false)
     }
-  }
-
-  const handleCityChange = (event: SelectChangeEvent<string[]>) => {
-    setSelectedCities(event.target.value as string[])
-  }
-
-  const handleRemoveCity = (cityToRemove: string) => {
-    setSelectedCities((prevCities) =>
-      prevCities.filter((city) => city !== cityToRemove),
-    )
   }
 
   const handleUpdateCities = async () => {
@@ -175,168 +147,135 @@ const UserProfile = () => {
   }
 
   return (
-    <Card
+    <Page
       sx={{
-        display: 'flex',
-        flexDirection: { xs: 'column', md: 'row' },
-        maxWidth: '100%',
-        overflow: 'hidden',
-        p: 3,
+        height: '100%',
       }}
     >
       <Box
         sx={{
-          width: { xs: '100%', md: '300px' },
-          textAlign: 'center',
-          borderRight: { md: '1px solid rgba(0, 0, 0, 0.12)' },
+          display: 'flex',
+          flexDirection: { xs: 'column', md: 'row' },
+          maxWidth: '100%',
+
           p: 3,
         }}
       >
-        <Grid container direction="column" alignItems="center" spacing={2}>
-          <Grid>
-            <img
-              src={avatar}
-              alt="Profile Picture"
-              style={{
-                width: '100px',
-                height: '100px',
-                borderRadius: '50%',
-                objectFit: 'cover',
-              }}
-            />
-          </Grid>
-          <Grid>
-            <Button
-              variant="contained"
-              component="label"
-              sx={{ mt: 1 }}
-              disabled={loading}
-            >
-              {loading ? <CircularProgress size={24} /> : 'Upload New Picture'}
-              <input
-                type="file"
-                hidden
-                accept="image/*"
-                onChange={handleAvatarChange}
+        <Box
+          sx={{
+            width: { xs: '100%', md: '300px' },
+            textAlign: 'center',
+            borderRight: { md: '1px solid rgba(0, 0, 0, 0.12)' },
+            p: 3,
+          }}
+        >
+          <Grid container direction="column" alignItems="center" spacing={2}>
+            <Grid>
+              <img
+                src={avatar}
+                alt="Profile Picture"
+                style={{
+                  width: '100px',
+                  height: '100px',
+                  borderRadius: '50%',
+                  objectFit: 'cover',
+                }}
               />
-            </Button>
-          </Grid>
+            </Grid>
+            <Grid>
+              <Button
+                variant="contained"
+                component="label"
+                sx={{ mt: 1 }}
+                disabled={loading}
+              >
+                {loading ? (
+                  <CircularProgress size={24} />
+                ) : (
+                  'Upload New Picture'
+                )}
+                <input
+                  type="file"
+                  hidden
+                  accept="image/*"
+                  onChange={handleAvatarChange}
+                />
+              </Button>
+            </Grid>
 
-          <Grid>
-            <Typography variant="body1">
-              Username: {(user?.unsafeMetadata?.userName as string) || 'N/A'}
-            </Typography>
-            <Typography variant="body1">
-              First Name: {user.firstName || 'N/A'}
-            </Typography>
-            <Typography variant="body1">
-              Last Name: {user.lastName || 'N/A'}
-            </Typography>
-            <Typography variant="body1">
-              Email: {user.primaryEmailAddress?.emailAddress}
-            </Typography>
+            <Grid>
+              <Typography variant="body1">
+                Username: {(user?.unsafeMetadata?.userName as string) || 'N/A'}
+              </Typography>
+              <Typography variant="body1">
+                First Name: {user.firstName || 'N/A'}
+              </Typography>
+              <Typography variant="body1">
+                Last Name: {user.lastName || 'N/A'}
+              </Typography>
+              <Typography variant="body1">
+                Email: {user.primaryEmailAddress?.emailAddress}
+              </Typography>
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
+        </Box>
 
-      <Box sx={{ flex: 1, minWidth: 0, p: 3 }}>
-        <Grid container direction="column" spacing={3}>
-          <Grid>
-            <Typography variant="h5" gutterBottom>
+        <Box sx={{ flex: 1, minWidth: 0, p: 3 }}>
+          <Box sx={{ mb: 2 }}>
+            <Typography variant="body1" gutterBottom>
               Select Expertise Tags
             </Typography>
-            <Select
-              label="Expertise Tags"
-              fullWidth
-              onChange={handleTagChange}
-              margin="dense"
-              value={selectedTags || []}
-              multiple
-              renderValue={(selected) => (selected as string[]).join(', ')}
-            >
-              {Array.isArray(tags) &&
-                tags.map((tag) => (
-                  <MenuItem key={tag.id} value={tag.name}>
-                    {tag.name}
-                  </MenuItem>
-                ))}
-            </Select>
-            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1, mt: 2 }}>
-              {selectedTags?.map((tag) => (
-                <Chip
-                  key={tag}
-                  label={tag}
-                  onDelete={() => handleRemoveTag(tag)}
-                />
-              ))}
-            </Box>
+
+            <TagSelector
+              selectedTags={selectedTags}
+              setSelectedTags={setSelectedTags}
+            />
+
             <Button
               variant="contained"
-              fullWidth
               onClick={handleUpdateTags}
               disabled={loading}
-              sx={{ mt: 2 }}
             >
               {loading ? <CircularProgress size={24} /> : 'Update Tags'}
             </Button>
-          </Grid>
-          <Grid>
-            <Typography variant="h5" gutterBottom>
+          </Box>
+          <Box sx={{ mb: 2 }}>
+            <Typography variant="body1" gutterBottom>
               Select City Tags
             </Typography>
-            <Select
-              label="Cities"
-              fullWidth
-              onChange={handleCityChange}
-              margin="dense"
-              value={selectedCities || []}
-              multiple
-              renderValue={(selected) => (selected as string[]).join(', ')}
-            >
-              {Array.isArray(cities) &&
-                cities.map((city: City) => (
-                  <MenuItem key={city.id} value={city.city}>
-                    {city.city}
-                  </MenuItem>
-                ))}
-            </Select>
-            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1, mt: 2 }}>
-              {selectedCities?.map((city) => (
-                <Chip
-                  key={city}
-                  label={city}
-                  onDelete={() => handleRemoveCity(city)}
-                />
-              ))}
-            </Box>
+            <TagSelector
+              selectedTags={selectedCities}
+              setSelectedTags={setSelectedCities}
+              cityType
+            />
+
             <Button
               variant="contained"
-              fullWidth
               onClick={handleUpdateCities}
               disabled={loading}
               sx={{ mt: 2 }}
             >
               {loading ? <CircularProgress size={24} /> : 'Update Tags'}
             </Button>
-          </Grid>
-        </Grid>
-      </Box>
+          </Box>
+        </Box>
 
-      <Snackbar
-        open={alert.open}
-        autoHideDuration={6000}
-        onClose={handleCloseAlert}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-      >
-        <Alert
+        <Snackbar
+          open={alert.open}
+          autoHideDuration={6000}
           onClose={handleCloseAlert}
-          severity={alert.severity}
-          sx={{ width: '100%' }}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
         >
-          {alert.message}
-        </Alert>
-      </Snackbar>
-    </Card>
+          <Alert
+            onClose={handleCloseAlert}
+            severity={alert.severity}
+            sx={{ width: '100%' }}
+          >
+            {alert.message}
+          </Alert>
+        </Snackbar>
+      </Box>
+    </Page>
   )
 }
 
