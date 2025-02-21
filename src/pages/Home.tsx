900601913f983b9e99f3d72ea9c06d585287a479
Tarik <tarikboumehdi91@gmail.com>
2025-02-21 13:03:05 -0700

    risking for the demo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,18 @@
-import { Box, Typography, Button, IconButton, useTheme, useMediaQuery } from '@mui/material';
-import { useState, useEffect, useRef } from 'react';
+import { Box, Typography, Button, IconButton, useTheme, useMediaQuery, Fab, Drawer } from '@mui/material';
+import { useState, useEffect, useRef, useCallback } from 'react';
 import FirstSigninFlow from '../components/FirstSigninFlow/FirstSigninFlow';
 import { useUser } from '@clerk/clerk-react';
 import { useSearchQuery } from '../hooks/useSearchQuery';
 import { Page } from '../components/layout/Page';
 import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
-
+import { FaChevronLeft } from "react-icons/fa";
+import Leaderboard from '../components/Leaderboard';
 const Home: React.FC = () => {
   const user = useUser();
   const [isFirstSignIn, setIsFirstSignIn] = useState(
     Boolean(user.user?.unsafeMetadata.isFirstSignIn)
   );
-
+  
   const handleTestClick = () => {
     setIsFirstSignIn(true);
   };
@@ -19,15 +20,18 @@ const Home: React.FC = () => {
   const { questions, setParams } = useSearchQuery();
   const userTags = user.user?.unsafeMetadata.tags as string | undefined;
   const scrollContainerRef = useRef<HTMLDivElement | null>(null);
-
+  const [openDrawer, setOpenDrawer] = useState(open !== null && open !== undefined); // initialize openDrawer to true if open is not null or undefined
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm')); // Detect small screens
-
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpenDrawer(newOpen);
+  };
   useEffect(() => {
     setParams({ tags: userTags });
   }, []); // eslint-disable-line
 
-  const scroll = (direction: 'left' | 'right') => {
+
+  const scroll = useCallback((direction: 'left' | 'right') => {
     if (scrollContainerRef.current) {
       const scrollAmount = isMobile ? 150 : 320;
       const currentScrollPosition = scrollContainerRef.current.scrollLeft;
@@ -36,16 +40,22 @@ const Home: React.FC = () => {
     } else {
       console.error('scrollContainerRef is not attached');
     }
-  };
+  }, [isMobile, scrollContainerRef]);
 
   useEffect(() => {
     const autoScroll = setInterval(() => {
       scroll('right');
     }, isMobile ? 5000 : 3000);
     return () => clearInterval(autoScroll);
-  }, [isMobile]);
+  }, [isMobile, scroll]);
 
-  return (
+  return (<>
+    { !openDrawer &&
+      <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+      <Fab  sx={{ position: "fixed", bottom: 16, right: 16 }} size="medium" aria-label="add" onClick={toggleDrawer(true)}>
+      <FaChevronLeft />
+    </Fab>
+    </Box>}
     <Page sx={{ height: '100%', padding: 2 }}>
       <Box sx={{ marginTop: 4, paddingX: isMobile ? 1 : 4 }}>
         <FirstSigninFlow
@@ -107,8 +117,12 @@ const Home: React.FC = () => {
             </IconButton>
           )}
         </Box>
-      </Box>
-    </Page>
+      </Box> {/* desktop mode Leaderboard */}
+        <Drawer open={openDrawer} onClose={toggleDrawer(false)} anchor="right">
+      <Leaderboard />
+    </Drawer>
+
+    </Page></>
   );
 };
 
