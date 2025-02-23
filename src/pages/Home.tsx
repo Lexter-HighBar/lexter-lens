7847811b5cfb3437745120e450a2b88784af9995
Petra Petlanova <petrapetlanova@gmail.com>
2025-02-22 17:47:47 -0700

    Added BodyHome component. Made a few changes to UI. (#81)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,129 +1,168 @@
-import { Box, Typography, Button, IconButton, useTheme, useMediaQuery, Fab, Drawer } from '@mui/material';
-import { useState, useEffect, useRef, useCallback } from 'react';
-import FirstSigninFlow from '../components/FirstSigninFlow/FirstSigninFlow';
-import { useUser } from '@clerk/clerk-react';
-import { useSearchQuery } from '../hooks/useSearchQuery';
-import { Page } from '../components/layout/Page';
-import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
-import { FaChevronLeft } from "react-icons/fa";
-import Leaderboard from '../components/Leaderboard';
+import {
+  Box,
+  Typography,
+  Button,
+  IconButton,
+  useTheme,
+  useMediaQuery,
+  Fab,
+  Drawer,
+} from '@mui/material'
+import { useState, useEffect, useRef, useCallback } from 'react'
+import FirstSigninFlow from '../components/FirstSigninFlow/FirstSigninFlow'
+import { useUser } from '@clerk/clerk-react'
+import { useSearchQuery } from '../hooks/useSearchQuery'
+import { Page } from '../components/layout/Page'
+import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material'
+import { FaChevronLeft } from 'react-icons/fa'
+import Leaderboard from '../components/Leaderboard'
+import BodyHome from '../components/BodyHome'
+
 const Home: React.FC = () => {
-  const user = useUser();
+  const user = useUser()
   const [isFirstSignIn, setIsFirstSignIn] = useState(
-    Boolean(user.user?.unsafeMetadata.isFirstSignIn)
-  );
-  
+    Boolean(user.user?.unsafeMetadata.isFirstSignIn),
+  )
+
   const handleTestClick = () => {
-    setIsFirstSignIn(true);
-  };
+    setIsFirstSignIn(true)
+  }
 
-  const { questions, setParams } = useSearchQuery();
-  const userTags = user.user?.unsafeMetadata.tags as string | undefined;
-  const scrollContainerRef = useRef<HTMLDivElement | null>(null);
-  const [openDrawer, setOpenDrawer] = useState(open !== null && open !== undefined); // initialize openDrawer to true if open is not null or undefined
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm')); // Detect small screens
+  const { questions, setParams } = useSearchQuery()
+  const userTags = user.user?.unsafeMetadata.tags as string | undefined
+  const scrollContainerRef = useRef<HTMLDivElement | null>(null)
+  const [openDrawer, setOpenDrawer] = useState(
+    open !== null && open !== undefined,
+  ) // initialize openDrawer to true if open is not null or undefined
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm')) // Detect small screens
   const toggleDrawer = (newOpen: boolean) => () => {
-    setOpenDrawer(newOpen);
-  };
+    setOpenDrawer(newOpen)
+  }
   useEffect(() => {
-    setParams({ tags: userTags });
-  }, []); // eslint-disable-line
+    setParams({ tags: userTags })
+  }, []) // eslint-disable-line
 
-
-  const scroll = useCallback((direction: 'left' | 'right') => {
-    if (scrollContainerRef.current) {
-      const scrollAmount = isMobile ? 150 : 320;
-      const currentScrollPosition = scrollContainerRef.current.scrollLeft;
-      const newScrollPosition = direction === 'left' ? currentScrollPosition - scrollAmount : currentScrollPosition + scrollAmount;
-      scrollContainerRef.current.scrollTo({ left: newScrollPosition, behavior: 'smooth' });
-    } else {
-      console.error('scrollContainerRef is not attached');
-    }
-  }, [isMobile, scrollContainerRef]);
+  const scroll = useCallback(
+    (direction: 'left' | 'right') => {
+      if (scrollContainerRef.current) {
+        const scrollAmount = isMobile ? 150 : 320
+        const currentScrollPosition = scrollContainerRef.current.scrollLeft
+        const newScrollPosition =
+          direction === 'left'
+            ? currentScrollPosition - scrollAmount
+            : currentScrollPosition + scrollAmount
+        scrollContainerRef.current.scrollTo({
+          left: newScrollPosition,
+          behavior: 'smooth',
+        })
+      } else {
+        console.error('scrollContainerRef is not attached')
+      }
+    },
+    [isMobile, scrollContainerRef],
+  )
 
   useEffect(() => {
-    const autoScroll = setInterval(() => {
-      scroll('right');
-    }, isMobile ? 5000 : 3000);
-    return () => clearInterval(autoScroll);
-  }, [isMobile, scroll]);
+    const autoScroll = setInterval(
+      () => {
+        scroll('right')
+      },
+      isMobile ? 5000 : 3000,
+    )
+    return () => clearInterval(autoScroll)
+  }, [isMobile, scroll])
 
-  return (<>
-    { !openDrawer &&
-      <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-      <Fab  sx={{ position: "fixed", bottom: 16, right: 16 }} size="medium" aria-label="add" onClick={toggleDrawer(true)}>
-      <FaChevronLeft />
-    </Fab>
-    </Box>}
-    <Page sx={{ height: '100%', padding: 2 }}>
-      <Box sx={{ marginTop: 4, paddingX: isMobile ? 1 : 4 }}>
-        <FirstSigninFlow
-          isFirstSignIn={isFirstSignIn}
-          setIsFirstSignIn={setIsFirstSignIn}
-        />
-        <Button onClick={handleTestClick} sx={{ mb: 2 }}>Test</Button>
-        
-        <Typography variant="h6" gutterBottom>
-          Questions Based on Your Interests
-        </Typography>
-
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-          {!isMobile && (
-            <IconButton onClick={() => scroll('left')}>
-              <ArrowBackIos />
-            </IconButton>
-          )}
-
-          <Box
-            ref={scrollContainerRef}
-            sx={{
-              display: 'flex',
-              overflowX: 'auto',
-              scrollBehavior: 'smooth',
-              gap: 2,
-              padding: 2,
-              width: '100%',
-              maxWidth: isMobile ? '100%' : 750,
-              '&::-webkit-scrollbar': { display: 'none' }, // Hide scrollbar
-            }}
+  return (
+    <>
+      {!openDrawer && (
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <Fab
+            sx={{ position: 'fixed', top: 130, right: 30 }}
+            variant="extended"
+            size="medium"            
+            onClick={toggleDrawer(true)}
           >
-            {questions.length > 0 ? (
-              questions.map((q) => (
-                <Box
-                  key={q.QuestionId}
-                  sx={{
-                    minWidth: isMobile ? 100 : 400, // Adjust width for mobile
-                    padding: 2,
-                    backgroundColor: '#f9f9f9',
-                    borderRadius: 2,
-                    boxShadow: 1,
-                  }}
-                >
-                  <Typography variant="h6">{q.userName}</Typography>
-                  <Typography variant="body2" color="textSecondary">
-                    {q.content}
-                  </Typography>
-                </Box>
-              ))
-            ) : (
-              <Typography>No questions found based on your tags.</Typography>
+            <FaChevronLeft style={{ marginRight: 4 }}/>
+            Leaderboard
+          </Fab>
+        </Box>
+      )}
+
+      <Page sx={{ height: '100%', padding: 2 }}>
+        <Box sx={{ width: '100%', height: '100%',}}>
+          <BodyHome />
+        </Box>
+        <Box sx={{ mt: 4, paddingX: isMobile ? 1 : 4 }}>
+          <FirstSigninFlow
+            isFirstSignIn={isFirstSignIn}
+            setIsFirstSignIn={setIsFirstSignIn}
+          />
+          <Button onClick={handleTestClick} sx={{ mb: 4 }}>
+            Quickstart Test
+          </Button>
+
+          <Typography variant="h6" gutterBottom>
+            Insights Based on Your Interests
+          </Typography>
+
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1, mb: 10 }}>
+            {!isMobile && (
+              <IconButton onClick={() => scroll('left')}>
+                <ArrowBackIos />
+              </IconButton>
+            )}
+
+            <Box
+              ref={scrollContainerRef}
+              sx={{
+                display: 'flex',
+                overflowX: 'auto',
+                scrollBehavior: 'smooth',
+                gap: 2,
+                padding: 2,
+                width: '100%',
+                maxWidth: isMobile ? '100%' : 750,
+                '&::-webkit-scrollbar': { display: 'none' }, // Hide scrollbar
+              }}
+            >
+              {questions.length > 0 ? (
+                questions.map((q) => (
+                  <Box
+                    key={q.QuestionId}
+                    sx={{
+                      minWidth: isMobile ? 100 : 400, // Adjust width for mobile
+                      padding: 2,
+                      backgroundColor: 'primary.contrastText',
+                      borderRadius: 2,
+                      boxShadow: 1,
+                    }}
+                  >
+                    <Typography variant="h6">{q.userName}</Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      {q.content}
+                    </Typography>
+                  </Box>
+                ))
+              ) : (
+                <Typography>No questions found based on your tags.</Typography>
+              )}
+            </Box>
+
+            {!isMobile && (
+              <IconButton onClick={() => scroll('right')}>
+                <ArrowForwardIos />
+              </IconButton>
             )}
           </Box>
-
-          {!isMobile && (
-            <IconButton onClick={() => scroll('right')}>
-              <ArrowForwardIos />
-            </IconButton>
-          )}
-        </Box>
-      </Box> {/* desktop mode Leaderboard */}
+        </Box>{' '}
+        {/* desktop mode Leaderboard */}
         <Drawer open={openDrawer} onClose={toggleDrawer(false)} anchor="right">
-      <Leaderboard />
-    </Drawer>
+          <Leaderboard />
+        </Drawer>
+      </Page>
+    </>
+  )
+}
 
-    </Page></>
-  );
-};
-
-export default Home;
+export default Home
