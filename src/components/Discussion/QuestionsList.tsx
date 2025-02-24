fbf41174cbe318520c3328b3ad277a22eb3901f9
Hank Chen <hychen958@gmail.com>
2025-02-24 11:51:05 -0700

    filter final fix 5 (#90)

--- src/components/Discussion/QuestionsList.tsx
+++ src/components/Discussion/QuestionsList.tsx
@@ -1,35 +1,35 @@
-import QuestionItem from './QuestionItem'
-
-import { Question } from '../../lib/types'
-
-import { useQuestions } from '../../hooks/useQuestions'
-import { Box } from '@mui/material'
-import { LoadingSkelton } from './LoadingSkelton'
+import QuestionItem from './QuestionItem';
+import { Question } from '../../lib/types';
+import { Box } from '@mui/material';
 
 interface Props {
-  questions: Question[]
+  questions: Question[];
+  filter: string; // Added filter prop
 }
 
-const QuestionsList = ({ questions }: Props) => {
-  const { loading, error } = useQuestions()
-
-  if (loading) {
-    return (
-      <>
-        <LoadingSkelton />
-      </>
-    )
-  } else if (error) {
-    return <Box p={6}>Error loading questions</Box>
-  }
+const QuestionsList = ({ questions, filter }: Props) => {
+  // Sort and filter questions on the frontend
+  const sortedQuestions = [...questions].sort((a, b) => {
+    if (filter === 'new') {
+      return new Date(b.createdOn).getTime() - new Date(a.createdOn).getTime();
+    }
+    if (filter === 'relevant') {
+      return b.tags.length - a.tags.length; // Sort by tag count
+    }
+    if (filter === 'trending') {
+      return Math.random() - 0.5; // Placeholder for trending logic
+    }
+    return 0;
+  });
 
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
-      {questions.map((question) => (
+      {sortedQuestions.map((question) => (
         <QuestionItem key={question.QuestionId} question={question} />
       ))}
     </Box>
-  )
-}
+  );
+};
 
-export default QuestionsList
+export default QuestionsList;
+
