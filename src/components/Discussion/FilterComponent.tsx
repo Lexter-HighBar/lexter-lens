a58b5e6c588a5822aec7283edfc30dae53f7040b
hychen958 <hychen958@gmail.com>
2025-02-24 11:37:39 -0700

    filter final fix 5

---
+++ src/components/Discussion/FilterComponent.tsx
@@ -0,0 +1,45 @@
+
+import { useState } from 'react'
+import { Box, Menu, MenuItem, IconButton } from '@mui/material';
+import FilterListIcon from '@mui/icons-material/FilterList';
+const filterOptions = [
+  { value: 'new', label: 'New' },
+  { value: 'relevant', label: 'Relevant' },
+  { value: 'trending', label: 'Trending' },
+];
+interface FilterProps {
+  onFilterChange: (filter: string) => void;
+}
+const FilterComponent = ({ onFilterChange }: FilterProps) => {
+  const [selectedFilter, setSelectedFilter] = useState('new');
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  
+  const handleOpenMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleCloseMenu = (newFilter?: string) => {
+    setAnchorEl(null);
+    if (newFilter) {
+      setSelectedFilter(newFilter);
+      onFilterChange(newFilter);
+    }
+  };
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', width: '100%', marginBottom: '10px' }}>
+      <Box sx={{ marginRight: '10px' }}>
+        {filterOptions.find(option => option.value === selectedFilter)?.label}
+      </Box>
+      <IconButton onClick={handleOpenMenu}>
+        <FilterListIcon />
+      </IconButton>
+      <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={() => handleCloseMenu()}>
+        {filterOptions.map((option) => (
+          <MenuItem key={option.value} onClick={() => handleCloseMenu(option.value)}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </Menu>
+    </Box>
+  );
+};
+export default FilterComponent;
--- src/pages/Discussion.tsx
+++ src/pages/Discussion.tsx
@@ -1,32 +1,38 @@
-import { Page } from '../components/layout/Page'
-
-import QuestionsList from '../components/Discussion/QuestionsList'
-import { Question } from '../lib/types'
-import { useQuestions } from '../hooks/useQuestions'
-import CreateQuestion from '../components/Discussion/CreateQuestion'
-import { Box } from '@radix-ui/themes'
+import { Page } from '../components/layout/Page';
+import QuestionsList from '../components/Discussion/QuestionsList';
+import { Question } from '../lib/types';
+import { useQuestions } from '../hooks/useQuestions';
+import CreateQuestion from '../components/Discussion/CreateQuestion';
+import FilterComponent from '../components/Discussion/FilterComponent';
+import { Box } from '@mui/material';
+import { useState } from 'react';
 
 // Discussion page component
 export const Discussion = () => {
-  // State to manage all questions
-  const { questions } = useQuestions()
-  return (
-    <Page
-      sx={{
-        height: '100%',
-        overflowY: 'auto',
-        overflowX: 'hidden',
-      }}
-    >
-      {/* Dialog to create a new question */}
-      <Box> 
-      <CreateQuestion  />
-      </Box>
-      
-      <QuestionsList  questions={questions as Question[]} />
-    
-    </Page>
-  )
-}
+  // Fetch all questions
+  const { questions } = useQuestions();
+  
+  // Manage selected filter state
+  const [filter, setFilter] = useState('new');
 
-export default Discussion
+  // Handle filter change from the dropdown menu
+  const handleFilterChange = (newFilter: string) => {
+    setFilter(newFilter);
+  };
+
+  return (
+    <Page sx={{ height: '100%', overflowY: 'auto', overflowX: 'hidden' }}>
+      <Box>
+        <CreateQuestion />
+        {/* Filter dropdown component */}
+        <FilterComponent onFilterChange={handleFilterChange} />
+      </Box>
+
+      {/* Pass filter state to the questions list */}
+      <QuestionsList questions={questions as Question[]} filter={filter} />
+    </Page>
+  );
+};
+
+export default Discussion;
+
