4a026db8a36096ddfebc7bbf533aba315bc86867
hychen958 <hychen958@gmail.com>
2025-02-23 20:35:57 -0700

    filter final

--- src/pages/Discussion.tsx
+++ src/pages/Discussion.tsx
@@ -1,35 +1,38 @@
-import { Page } from '../components/layout/Page'
-import QuestionsList from '../components/Discussion/QuestionsList'
-import { Question } from '../lib/types'
-import { useQuestions } from '../hooks/useQuestions'
-import CreateQuestion from '../components/Discussion/CreateQuestion'
+import { Page } from '../components/layout/Page';
+import QuestionsList from '../components/Discussion/QuestionsList';
+import { Question } from '../lib/types';
+import { useQuestions } from '../hooks/useQuestions';
+import CreateQuestion from '../components/Discussion/CreateQuestion';
 import FilterComponent from '../components/Discussion/FilterComponent';
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import { useState } from 'react';
+
 // Discussion page component
 export const Discussion = () => {
-  // State to manage all questions
+  // Fetch all questions
   const { questions } = useQuestions();
+  
+  // Manage selected filter state
   const [filter, setFilter] = useState('new');
+
+  // Handle filter change from the dropdown menu
   const handleFilterChange = (newFilter: string) => {
     setFilter(newFilter);
   };
+
   return (
-    <Page
-      sx={{
-        height: '100%',
-        overflowY: 'auto',
-        overflowX: 'hidden',
-      }}
-    >
+    <Page sx={{ height: '100%', overflowY: 'auto', overflowX: 'hidden' }}>
       <Box>
         <CreateQuestion />
-        {/* add filter dropdown */}
+        {/* Filter dropdown component */}
         <FilterComponent onFilterChange={handleFilterChange} />
       </Box>
-      
+
+      {/* Pass filter state to the questions list */}
       <QuestionsList questions={questions as Question[]} filter={filter} />
     </Page>
   );
 };
+
 export default Discussion;
+
