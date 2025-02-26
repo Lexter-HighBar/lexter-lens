aa2d8b59d89e95ad0d2ab7f3fc1770fc1af3bb3a
Hank Chen <hychen958@gmail.com>
2025-02-26 09:41:59 -0700

    Revert "leaderboard on discussion (#93)" (#94)
    This reverts commit 42f14b776d9d0a36bd71360cb8fbb7cb5f168262.

--- src/pages/Discussion.tsx
+++ src/pages/Discussion.tsx
@@ -3,11 +3,7 @@ import QuestionsList from '../components/Discussion/QuestionsList';
 import { Question } from '../lib/types';
 import { useQuestions } from '../hooks/useQuestions';
 import CreateQuestion from '../components/Discussion/CreateQuestion';
-import FilterComponent from '../components/Discussion/FilterComponent';
-import { Box, Fab, Drawer, IconButton, Typography } from '@mui/material';
-import { ChevronLeft, Close } from '@mui/icons-material';
 import { useState } from 'react';
-import Leaderboard from '../components/Leaderboard';
 
 // Discussion page component
 export const Discussion = () => {
@@ -18,65 +14,18 @@ export const Discussion = () => {
   const [filter, setFilter] = useState('new');
 
   // Handle filter change from the dropdown menu
-  const handleFilterChange = (newFilter: string) => {
-    setFilter(newFilter);
-  };
  
 
-  // State for Leaderboard Drawer
-  const [openDrawer, setOpenDrawer] = useState(false);
-  
-  const toggleDrawer = (newOpen: boolean) => () => {
-    setOpenDrawer(newOpen);
-  };
-
   return (
-    <>
-      {/* Leaderboard Button */}
-      {!openDrawer && (
-        <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-          <Fab
-            sx={{ position: 'fixed', top: 130, right: 30 }}
-            variant="extended"
-            size="medium"
-            onClick={toggleDrawer(true)}
-          >
-            <ChevronLeft style={{ marginRight: 4 }} />
-            Leaderboard
-          </Fab>
-        </Box>
-      )}
+    <Page sx={{ height: '100%', overflowY: 'auto', overflowX: 'hidden' }}>
+      
+        <CreateQuestion setFilter={setFilter} />
+        {/* Filter dropdown component */}
+    
 
-      {/* Leaderboard Drawer */}
-      <Drawer
-        variant="persistent"
-        sx={{ backgroundColor: 'primary.dark' }}
-        open={openDrawer}
-        onClose={toggleDrawer(false)}
-        anchor="right"
-      >
-        <Box component="div" p={2} display="flex" justifyContent="space-between" sx={{ backgroundColor: 'primary.dark' }}>
-          <IconButton onClick={toggleDrawer(false)} sx={{ color: 'white' }}>
-            <Close />
-          </IconButton>
-          <Typography variant="h2" gutterBottom color="primary.contrastText">
-            Leaderboard
-          </Typography>
-        </Box>
-        <Leaderboard />
-      </Drawer>
-
-      <Page sx={{ height: '100%', overflowY: 'auto', overflowX: 'hidden' }}>
-        <Box>
-          <CreateQuestion />
-          {/* Filter dropdown component */}
-          <FilterComponent onFilterChange={handleFilterChange} />
-        </Box>
-
-        {/* Pass filter state to the questions list */}
-        <QuestionsList questions={questions as Question[]} filter={filter} />
-      </Page>
-    </>
+      {/* Pass filter state to the questions list */}
+      <QuestionsList questions={questions as Question[]} filter={filter} />
+    </Page>
   );
 };
 
