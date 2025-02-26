b6e4c862c03220d720b13361790a42e879dc9fd3
Hank Chen <hychen958@gmail.com>
2025-02-26 10:12:43 -0700

    Lxdv h (#95)
    * leaderboard on discussion
    
    * leaderbaoard for discussion final
    
    * leaderboard button grand final

--- src/pages/Discussion.tsx
+++ src/pages/Discussion.tsx
@@ -4,6 +4,9 @@ import { Question } from '../lib/types';
 import { useQuestions } from '../hooks/useQuestions';
 import CreateQuestion from '../components/Discussion/CreateQuestion';
 import { useState } from 'react';
+import { Box, Fab, Drawer, IconButton, Typography } from '@mui/material';
+import { ChevronLeft, Close } from '@mui/icons-material';
+import Leaderboard from '../components/Leaderboard';
 
 // Discussion page component
 export const Discussion = () => {
@@ -13,21 +16,56 @@ export const Discussion = () => {
   // Manage selected filter state
   const [filter, setFilter] = useState('new');
 
-  // Handle filter change from the dropdown menu
- 
+  // State for Leaderboard Drawer
+  const [openDrawer, setOpenDrawer] = useState(false);
+  
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpenDrawer(newOpen);
+  };
 
   return (
-    <Page sx={{ height: '100%', overflowY: 'auto', overflowX: 'hidden' }}>
-      
-        <CreateQuestion setFilter={setFilter} />
-        {/* Filter dropdown component */}
-    
+    <>
+      {/* Leaderboard Button */}
+      {!openDrawer && (
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <Fab
+            sx={{ position: 'fixed', top: 130, right: 30 }}
+            variant="extended"
+            size="medium"
+            onClick={toggleDrawer(true)}
+          >
+            <ChevronLeft style={{ marginRight: 4 }} />
+            Leaderboard
+          </Fab>
+        </Box>
+      )}
 
-      {/* Pass filter state to the questions list */}
-      <QuestionsList questions={questions as Question[]} filter={filter} />
-    </Page>
+      {/* Leaderboard Drawer */}
+      <Drawer
+        variant="persistent"
+        sx={{ backgroundColor: 'primary.dark' }}
+        open={openDrawer}
+        onClose={toggleDrawer(false)}
+        anchor="right"
+      >
+        <Box component="div" p={2} display="flex" justifyContent="space-between" sx={{ backgroundColor: 'primary.dark' }}>
+          <IconButton onClick={toggleDrawer(false)} sx={{ color: 'white' }}>
+            <Close />
+          </IconButton>
+          <Typography variant="h2" gutterBottom color="primary.contrastText">
+            Leaderboard
+          </Typography>
+        </Box>
+        <Leaderboard />
+      </Drawer>
+
+      <Page sx={{ height: '100%', overflowY: 'auto', overflowX: 'hidden' }}>
+        <CreateQuestion setFilter={setFilter} />
+        {/* Pass filter state to the questions list */}
+        <QuestionsList questions={questions as Question[]} filter={filter} />
+      </Page>
+    </>
   );
 };
 
 export default Discussion;
-
