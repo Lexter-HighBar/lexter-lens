cdb75fbbae1920b6bd5577a6f3a8d724014b0988
Tarik <tarikboumehdi91@gmail.com>
2025-02-25 18:45:11 -0700

    fixing firms tags and styling discussion

--- src/pages/Discussion.tsx
+++ src/pages/Discussion.tsx
@@ -3,8 +3,6 @@ import QuestionsList from '../components/Discussion/QuestionsList';
 import { Question } from '../lib/types';
 import { useQuestions } from '../hooks/useQuestions';
 import CreateQuestion from '../components/Discussion/CreateQuestion';
-import FilterComponent from '../components/Discussion/FilterComponent';
-import { Box } from '@mui/material';
 import { useState } from 'react';
 
 // Discussion page component
@@ -16,17 +14,14 @@ export const Discussion = () => {
   const [filter, setFilter] = useState('new');
 
   // Handle filter change from the dropdown menu
-  const handleFilterChange = (newFilter: string) => {
-    setFilter(newFilter);
-  };
+ 
 
   return (
     <Page sx={{ height: '100%', overflowY: 'auto', overflowX: 'hidden' }}>
-      <Box>
-        <CreateQuestion />
+      
+        <CreateQuestion setFilter={setFilter} />
         {/* Filter dropdown component */}
-        <FilterComponent onFilterChange={handleFilterChange} />
-      </Box>
+    
 
       {/* Pass filter state to the questions list */}
       <QuestionsList questions={questions as Question[]} filter={filter} />
